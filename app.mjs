821dda90f5fb9cff5c3eb507c24258cf1000a51f
utplaksh mishra <umishra8770@gmail.com>
2025-03-08 15:25:36 +0530

    Added a code that opens and read the command.txt

--- app.mjs
+++ app.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const cmdFilePath = path.join(__dirname, "command.txt");
 // console.log(
 //   `The output for import.meta.url is: ${
 //     import.meta.url
@@ -14,12 +14,14 @@ const __dirname = path.dirname(__filename);
 // Checking and Creating if "cammand.txt" dont exists
 (async () => {
   try {
-    await fs.access("command.txt");
+    await fs.access(cmdFilePath);
     console.log(`The command.txt file allready exists`);
   } catch (err) {
-    console.log(`command.txt not found! creating the file`);
+    console.log(
+      `command.txt not found! creatin"**/command.txt": trueg the file`
+    );
     try {
-      await fs.writeFile("command.txt", "");
+      await fs.writeFile(cmdFilePath, "");
       console.log(`command.txt created sucessfully`);
     } catch (err) {
       console.error(err);
@@ -29,13 +31,34 @@ const __dirname = path.dirname(__filename);
 
 // START watching the command.txt file for any changes
 (async () => {
-  const watcher = fs.watch("./command.txt");
+  const cmdFileHandler = await fs.open(cmdFilePath, "r"); // to open the file; here flag "r" means only read the file
+  const watcher = fs.watch(cmdFilePath);
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      // there is some change in the file
-      console.log(`A change was detected in the ${event.filename} file`);
+      // Detecting  some change in the file
+      console.log(`\nA change was detected in the ${event.filename} file`);
+      // I want to read a file
+
+      //    first I have to open the file => open (id_no.) eg 28 ; id_no:File descriptor
+
+      // get the  size of the command.txt
+      let fileMetaData = await cmdFileHandler.stat();
+      let sizeOfFile = fileMetaData.size;
+
+      // Allocate our buff with size of file
+      const buff = Buffer.alloc(sizeOfFile);
+      // the position at which we want to fill our buffer
+      const offset = 0;
+      // How many bytes we want to read
+      const length = buff.byteLength;
+      // The position that we want to start reading the file from
+      const position = 0;
+
+      //   We always want to read full content (from beginning-end)
+      const content = await cmdFileHandler.read(buff, offset, length, position);
+
+      console.log(content);
     }
-    // console.log(`The event in file is:`, event);
   }
 })();
